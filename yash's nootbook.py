39cf3e9e1a7fd17b046441d66f3418b279800225
Yash ashwani <63654783+yashashwani@users.noreply.github.com>
2020-05-09 23:23:28 +0530

    assignment 1

---
+++ yash's nootbook.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#assigning elements to different lists
+names = ['yash','jhon','steve','alex']
+print(names)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+names = ['yash','jhon','steve','alex']
+print(names)
+name1 = names.copy()
+name1.append('rock')
+print(name1)
+
+
+# In[10]:
+
+
+numbers = ['1','4','7']
+print(numbers)
+
+
+# In[13]:
+
+
+numbers = ['1','4','7']
+numbers[2]='9'
+print(numbers)
+numbers.insert(1,20)
+print(numbers)
+
+
+# In[17]:
+
+
+#accessing elements from a tuple.
+numbers = (1,4,8,9)
+print(numbers[2])
+
+
+# In[18]:
+
+
+coordinates = (1,4,8)
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+print(x,z)
+
+
+# In[22]:
+
+
+#deleting different dictionary elements.
+customer = {
+    "name":"yash ashwani",
+    "age":"19  ",
+    "is_certified":True
+}
+print(customer["name"])
+
+
+# In[23]:
+
+
+customer = {
+    "name":"yash ashwani",
+    "age":"19  ",
+    "is_certified":True
+}
+print(customer["name"])
+del customer["is_certified"]
+print(customer)
+
+
+# In[ ]:
+
+
+
+
